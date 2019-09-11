092488108e93adb5a05ae64b545024d13c67cade
Marcelo Perrella <marceloclperrella@gmail.com>
2019-09-11 19:01:12 -0300

    [feat] Add getDescription to Item

--- src/scrapers/Item.js
+++ src/scrapers/Item.js
@@ -18,6 +18,7 @@ module.exports = class Item extends Scraper {
             grade: this.getGrade(),
             name: parse(this.getName.bind(this)),
             type: parse(this.getType.bind(this)),
+            description: parse(this.getDescription.bind(this)),
         }
     }
 
@@ -46,4 +47,45 @@ module.exports = class Item extends Scraper {
             dreduction: Util.trim($('#dreduction').text()),
         }
     }
+
+    // Returns the item description if available.
+    getDescription(l, $ = this._parsers[l]) {
+        const keyword  = Util.getLangKeyword(l, 'DESCRIPTION')
+        const children = $('table.smallertext > tbody > tr:last-child > td').contents().toArray()
+        
+        const startIdx = children.findIndex(
+            node => node.type === 'text' && node.data.indexOf(keyword) > -1
+        ) + 1
+
+        let description = ''
+
+        for (let i = startIdx; i < children.length; i++) {
+            const { type, name, data } = children[i]
+
+            if (description) {
+                // Description always ends when there is a double line break.
+                if (type === 'tag' && name === 'br')
+                    if (i+1 < children.length && children[i+1].type === 'tag')
+                        break
+
+                // Description should end when a new HTML element is found and
+                // it already has found a description.
+                if (type === 'tag' && name !== 'br')
+                    break
+                
+                // If it starts with one of the special characters, then it's a
+                // new topic, or observation.
+                if (type === 'text' && (Util.startsWith(data, ['-', '–', '※']) || data.indexOf(':') > 0))
+                    break
+            } else if (name !== 'br') {
+                if (type === 'text')
+                    description += Util.trim(data) + '\n'
+                else
+                    description += Util.trim($(children[i]).text()) + '\n'
+            }
+        }
+        
+        return Util.trim(description) || null
+    }
+
 }
