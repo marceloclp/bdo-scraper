0c8dcf8b6b83327914ce8528767b858fe0be6798
Marcelo Perrella <marceloclperrella@gmail.com>
2019-09-12 07:40:19 -0300

    [refactor] Clean up unused tests

--- tests/test.js
+++
@@ -1,9 +0,0 @@
-const BDOScraper = require('./../src')
-const isEqual    = require('lodash').isEqual
-
-BDOScraper.load('https://bdocodex.com/us/item/9213/').then(scraper => {
-//BDOScraper.load('https://bdocodex.com/pt/recipe/122/').then(scraper => {
-    scraper.getRecipesFromItem().then(recipes => console.log(recipes))
-    //scraper.getRecipesFromItem()
-    //console.log(scraper.getRecipeExp())
-})
