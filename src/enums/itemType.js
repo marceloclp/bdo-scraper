53571c71065c0457806b9dd112551e645c7cbf07
Marcelo Perrella <marceloclp@users.noreply.github.com>
2019-10-04 17:59:30 +0100

    [fix] Change `consumivel` to `consumível' in enum 'itemType'

--- src/enums/itemType.js
+++ src/enums/itemType.js
@@ -10,7 +10,7 @@ const mapTypeToEnum = {
     ],
     pt: [
         'Equipamento',
-        'Consumivel',
+        'Consumível',
         'Item Especial',
         'Material necessário',
         'Geral',
